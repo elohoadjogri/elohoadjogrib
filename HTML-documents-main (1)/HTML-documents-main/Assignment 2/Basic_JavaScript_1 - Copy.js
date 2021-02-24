78965a7d98616f956a81533638c018b3dcf736e6
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-02-24 16:39:22 -0500

    Add files via upload

---
+++ HTML-documents-main (1)/HTML-documents-main/Assignment 2/Basic_JavaScript_1 - Copy.js
@@ -0,0 +1,8 @@
+<script>
+alert("Hello, World!");
+</script>
+function My First Function(){
+Var string="Kissme, I'm Irish!"'
+Var result=string.fontcolor("green");document.getElementByld("Irish").innerHTML=result;
+}
+
---
+++ HTML-documents-main (1)/HTML-documents-main/Assignment 2/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+function MyFirstFunction(){
+var string="Kissme; I'm Irish!";
+var result=string.fontcolor("green");document.getElementByld("Irish").innerHTML=result;
+}
+
+
