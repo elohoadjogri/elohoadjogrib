12a8d9c30268353c2215da572ad1b8ae1e9b9ab6
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-09-13 11:29:30 -0400

    Add files via upload

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,35 @@
+
+
+var blues= " I have the blues.";
+var blues= blues.fontcolor("blues");
+document.write(blues);
+
+  window.alert("Hello! I am an alert box!");
+
+var A = "This is a string";
+
+window.alert(A);
+
+let text = "John Doe";  // String written inside quotes
+document.getElementById("demo").innerHTML = text;
+
+let str1 = "Hello ";
+let str2 = "world!";
+document.getElementById("demo").innerHTML = str1.concat(str2);
+
+document.write("Lisa told Bart,\"knock it off, Bart! or I'll tell dada!\"<br> \" Eat my shorts!\" Bart responded.");
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+document.write("\" Be who your are and say what you feel."+"because those who mind don\'t matter and those who matter don\'t mind.\" "+"_Dr Seuss");
+
+var B = "Concatenated" + " string"
+document.write (B)
+
+family = "The Arezzinsis", Dad = "Jeremiah", mom ="Hermoine', Daughter = "Penny", Son = "Zoro";
+document.write(Dad);
+
+document.write(5+5);
