732aaacb732d6a15a89d3bffeaeea199dd034ffd
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-09-14 15:44:33 -0400

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z;
+
+let x = 10;
+x += 5;
+document.getElementById("demo").innerHTML = x;
