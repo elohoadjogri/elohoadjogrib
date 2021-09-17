fab381b66b10be51862d45576ee858e7491e846e
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-09-17 13:20:24 -0400

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,28 @@
+
+function myFunction() {
+    return Math.PI;
+  }
+      function Assignment(); {
+        document.getElementById("demo").innerHTML = "Hello World"();
+      }
+
+
+function subtraction_Function () {
+    var Subtraction = 8-3;
+    document.getElementById("Math").innerHTML = "8-3 = " + Subtraction;
+}
+
+function multiplication () {
+    var simple_Math = 7 * 3;
+    document.getElementById ("Math").innerHTML = "7x3= " + simple_Math;
+}
+     
+function division () {
+    var simple_Math = 35 / 7;
+    document.getElementById ("Math").innerHTML = "35/7= " + simple_Math;
+}  
+ 
+function more_math () {
+    var simple_Math = (2+2)*9/3-4;
+    document.getElementById ("Math").innerHTML = "2 plus 2, multiplied by 9, divided by 3 and then subtracted by 4 equals "+ simple_Math;
+}
