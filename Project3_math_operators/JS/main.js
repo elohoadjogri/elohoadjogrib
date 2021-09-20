e3711b2c9ec85376ae0dc1017b7243678c2863e3
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-09-20 10:19:03 -0400

    Add files via upload

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,28 +1,33 @@
 
-function myFunction() {
+function Math.random() {
     return Math.PI;
   }
-      function Assignment(); {
-        document.getElementById("demo").innerHTML = "Hello World"();
+      function Math.random(); {
+        document.getElementById("demo").innerHTML = "Math.random"();
       }
 
 
 function subtraction_Function () {
     var Subtraction = 8-3;
-    document.getElementById("Math").innerHTML = "8-3 = " + Subtraction;
+    document.getElementById("subtraction").innerHTML = "8-3 = " + Subtraction;
 }
 
 function multiplication () {
-    var simple_Math = 7 * 3;
-    document.getElementById ("Math").innerHTML = "7x3= " + simple_Math;
+    var multiplication = 7 * 3;
+    document.getElementById ("multiplication").innerHTML = "7x3= " + multiplication;
 }
      
 function division () {
-    var simple_Math = 35 / 7;
-    document.getElementById ("Math").innerHTML = "35/7= " + simple_Math;
+    var division = 35 / 7;
+    document.getElementById ("division").innerHTML = "35/7= " + division;
 }  
  
 function more_math () {
-    var simple_Math = (2+2)*9/3-4;
-    document.getElementById ("Math").innerHTML = "2 plus 2, multiplied by 9, divided by 3 and then subtracted by 4 equals "+ simple_Math;
+    var more_math = (2+2)*9/3-4;
+    document.getElementById ("more_math").innerHTML = "2 plus 2, multiplied by 9, divided by 3 and then subtracted by 4 equals "+ more_math;
+}
+
+function modulus_operator () {
+    var modulus_operator = 31 % 5;
+    document.getElementById("modulus_operator").innerHTML = "When you divide 31 by 5 you have a remainder of: " + modulus_operator;
 }
