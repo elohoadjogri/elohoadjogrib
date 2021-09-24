0b2dee4c2809bdf8c7beadd9fe6fa46dd0faf606
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-09-24 15:47:00 -0400

    Add files via upload

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_Dictionary() {
+    var Animal = {
+        Species:"Cat",
+        Color:"White",
+        Breed:"Scottish Fold",
+        Age:5,
+        Sound:"Meow!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = "Animal.Sound";   
+}
