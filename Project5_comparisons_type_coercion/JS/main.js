b076cbcbce81695cf12a44ecd6e14ebafdaa5bdd
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-10-04 12:30:40 -0400

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,56 @@
+document.write(typeof "Word");
+document.write(typeof 5);
+function infinity_Function() {
+    var infinity = 1.7976931348623157E308;
+    document.getElementById("infinity").innerHTML = "1.7976931348623157E308=" + infinity
+}
+
+function infinity_Function() {
+    var infinity = -3E310;
+    document.getElementById("infinity").innerHTML = "-3E310=" + infinity
+}
+  
+document.write(8>6);
+
+document.write(8<6);
+
+console.log(4+4);
+
+document.write("10"+8);
+
+document.write(6<2);
+console.log(5+3);
+
+document.write(12==12);
+
+document.write(5==13);
+
+x = 12;
+y = 12;
+document.write(x===y);
+
+x = 50;
+y = "50";
+document.write(x===y);
+
+A = "weather";
+B = "weather";
+document.write(A===B);
+
+A = season;
+B = "season";
+document.write(A===B);
+
+document.write(7>5 && 12>6);
+
+document.write(5>7 && 12>6);
+
+document.write(6>8 || 8>16);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(30>20);
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(10>20);
+}
