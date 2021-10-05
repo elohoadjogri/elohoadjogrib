2e2afa79128e4b0d241e95609cc1e2d844746b78
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-10-05 15:37:22 -0400

    Add files via upload

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -16,8 +16,8 @@ var Emily = new Vehicle("Jeep","Trial Hawk", 2019,"White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function Vehicle_Function()  {
     document.getElementById("Keywords_and_constructors").innerHTML =
-    "Erik drives a " + Erik.Vehicle_color + "-colored" + Erick. Vehicle_Model + 
-    "manufactured in " + Erik.Vehicle_Year;
+    "Erik drives a " + Erik.Vehicle_color + "-colored " + Erik. Vehicle_Model + 
+    " manufactured in " +  Erik.Vehicle_Year;
 }
 
 function Vehicle (Make, Model, Year, Color) {
@@ -31,11 +31,11 @@ var Emily = new Vehicle("Jeep","Trial Hawk", 2019,"White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function Vehicle_Function()  {
     document.getElementById("New_and_This").innerHTML =
-    "Erik drives a" + Erik.Vehicle_color + "-colored" + Erick. Vehicle_Model + 
-    "manufactured in" + Erik.Vehicle_Year;
+    "Erik drives a " + Erik.Vehicle_color + "-colored " + Erik. Vehicle_Model + 
+    " manufactured in" +  Erik.Vehicle_Year;
 }
 
-document.getElementById("demo").innerHTML = add();
+// document.getElementById("demo").innerHTML = add();
 function Nested_Function() {
   let counter = 0;
   function plus() {counter += 1;}
