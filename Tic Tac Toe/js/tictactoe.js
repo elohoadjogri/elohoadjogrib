846b1a2fd948c263827553695cab34f1c0cb01f5
elohoadjogri <74210819+elohoadjogri@users.noreply.github.com>
2021-11-09 16:16:15 -0500

    Add files via upload

--- Tic Tac Toe/js/tictactoe.js
+++ Tic Tac Toe/js/tictactoe.js
@@ -2,7 +2,7 @@
 let activePlayer = 'X';
 //This array stores an array of moves. We use this to determine win conditions.
 let selectedSquares = [];
-//This function is for p[lacing an x or o in a square.
+//This function is for placing an x or o in a square.
 function placeX0r0(squareNumber) {
   //This condition ensures a square hasn't be selected already.
   //This .some() method is used to check each element of selectedSquare array to
@@ -33,7 +33,7 @@ if (activePlayer === 'X') {
   activePlayer = 'X';
 }
 //This function plays placement sound.
-Audio('./media/place.mp3');
+audio('./media/place.mp3');
 //This condition checks to see if it is computers turn.
 if(activePlayer === 'O') {
   //This function disables clicking for computer choice.
@@ -57,9 +57,9 @@ function computersTurn () {
     //A random number between 0 and 8 is selected
     pickASquare = String(Math.floor(Math.random() * 9));
     //If the random number evaluates returns true, the square hasn't been selected yet.
-    if (placeXOrO(pickASquare)) {
+    if (placeX0r0(pickASquare)) {
       //This line calls the function.
-      placeXOrO(pickASquare);
+      placeX0r0(pickASquare);
       //This changes our boolean and ends the loop.
       success = true;
     };
@@ -71,7 +71,7 @@ function computersTurn () {
  //drawWinLine function is called to draw line if condition is met.
  function checkWinConditions() {
    //X 0, 1, 2 condition.
-   if    (arrayIncludes('0X', '1X', '2X')) {drawWinLine(50, 100, 558, 100); }
+   if    (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
    //X 3, 4, 5 condition.
    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
    //X 6, 7, 8 condition.
@@ -85,14 +85,14 @@ function computersTurn () {
    //X 6, 4, 2 condition.
    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); }
    //X 0, 4, 8 condition.
-   else if (arrayIncludes('OX', '4X', '8X')) { drawWinLine(100, 100, 520, 520); }
+   else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520); }
    //O 0, 1, 2 condition.
    else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100); }
    //X O 3, 4, 5 condition.
    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); }
    //X O 6, 7, 8 condition.
    else if (arrayIncludes('6O', '4O', '7O')) { drawWinLine(50, 508, 558,508); }
-   //X O O, 3, 6 condition.
+   //X O 0, 3, 6 condition.
    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
    //X O 1, 4, 7 condition.
    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
@@ -104,11 +104,11 @@ function computersTurn () {
    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
    //This condition checks for tie. If none of the above conditions register
    //and 9 squares are selected, the code executes.
-   else if (selectedSquares.length >= 9) {
+   else if (selectedSquares.length>= 9) {
      //This function plays the tie game sound.
-     Audio('./media/tie.mp3');
+     audio('./media/tie.mp3');
      //This function sets a .3 second timer before the resetGame is called.
-     setTimeout(function () {reserGame(); }, 1000);
+     setTimeout(function () {resetGame(); }, 1000);
    }
    
    //This function checks if an array includes 3 strings.
